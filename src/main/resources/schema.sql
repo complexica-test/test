c6717474e51c8abef53943afc737ecd222f51a31
Marcus SI <msi@complexica.com>
2020-10-29 11:09:13 +1030

    complexica-test udpate test codes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,3 @@
 create memory table if not exists test(ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(255));
-insert into test(name) values('hello');
-insert into test(name) values('world');
+insert into test(name) values('complexica');
+insert into test(name) values('developers');
